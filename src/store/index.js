ed88fecc4835eee6744ed94bbf068e942116fa52
Manucher <manucher20056@gmail.com>
2023-08-02 10:52:40 +0500

    '.'

--- src/store/index.js
+++ src/store/index.js
@@ -2,6 +2,8 @@ import { create } from 'zustand'
 
 export const useStore = create(set => ({
 	sendFiles: files => {
+		const url = 'https://cloud-api.yandex.net/v1/disk/resources/upload'
+
 		console.log(files)
 	},
 }))
